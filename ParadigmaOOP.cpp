861a57a356fda0e12ea0bf0674ac52a78e5f8e44
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-21 14:59:05 +0700

    deklarasi variabel nama dengan tipe data string

--- ParadigmaOOP.cpp
+++ ParadigmaOOP.cpp
@@ -5,5 +5,5 @@ using namespace std;
 class Mahasiswa {
 public: //Akses publik memungkinkan anggota kelas diakses dari luar kelas
     int nim; //deklarasi variabel nim dengan tipe data integer
-    
+    string nama; //deklarasi variabel nama dengan tipe data string
 };
