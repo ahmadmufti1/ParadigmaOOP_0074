b620b6472f59710a08da8fb2dcf650dbe8019a13
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-21 15:01:06 +0700

    Membuat fungsi printData untuk mencetak data mahasiswa

--- ParadigmaOOP.cpp
+++ ParadigmaOOP.cpp
@@ -7,5 +7,9 @@ public: //Akses publik memungkinkan anggota kelas diakses dari luar kelas
     int nim; //deklarasi variabel nim dengan tipe data integer
     string nama; //deklarasi variabel nama dengan tipe data string
     float nilai; //deklarasi variabel nilai dengan tipe data float
-   
+    void printData() {//fungsi untuk mencetak data mahasiswa
+        cout << "NIM: " << nim << endl;
+        cout << "Nama: " << nama << endl;
+        cout << "Nilai: " << nilai << endl;
+    }
 };
